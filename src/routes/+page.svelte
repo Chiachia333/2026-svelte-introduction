544e188fdad78c9901b075edcc9ab3ec5192282a
Chiara Geronimi <chiara.geronimi@mail.polimi.it>
2026-04-08 15:24:43 +0200

    commit make property derived

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,18 @@
 <script>
 let count = $state(0)
+let doubled = $derived(count * 2)
 console.log("Hello world");
-import ClicksCounter from "$lib/components/ClicksCounter.svelte";
-
-let count = $state(5);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClicksCounter bind:initialCount={count} />
-<p> count from parent is {count}</p>
-
-<button on:click={() => count++}>
+<button onclick={() => count++}>
 Clicks:{count}
 
 </button>
+
+<span> doubled is {doubled}</span>
 
 <style>
     h1 {
